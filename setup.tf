88bd3cbe65b3add7d5c8d869245b9f4da3cdaa5a
tomeroz802 <95613511+tomeroz802@users.noreply.github.com>
2024-01-27 19:22:14 +0200

    upload the eks terraform code
    upload the eks terraform code

---
+++ setup.tf
@@ -0,0 +1,52 @@
+resource "terraform_data" "clusterConfigutration" {
+  provisioner "local-exec" {
+    command = "aws eks --region eu-west-3 update-kubeconfig --name eks-cluster"
+  }
+  depends_on = [aws_eks_cluster.eks-cluster  ]
+}
+
+resource "terraform_data" "installIngress" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml"
+  }
+  depends_on = [terraform_data.clusterConfigutration]
+
+  
+}
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+  values           = [file("values/argocd.yaml")]
+
+  depends_on = [ terraform_data.installIngress]
+}
+# helm install argocd -n argocd -f values/argocd.yaml
+
+
+resource "terraform_data" "ApplyIngress" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f argo-ingress.yaml"
+  }
+
+  depends_on = [ helm_release.argocd ]
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

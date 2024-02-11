4f06d4b3e09dad2a0ed8cb6b063087e22c434e5f
tomeroz802 <95613511+tomeroz802@users.noreply.github.com>
2024-02-12 00:29:42 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,9 +10,9 @@ variable "networking" {
   })
   default = {
     cidr_block      = "141.0.0.0/16"
-    region          = "ap-northeast-3"
+    region          = "ap-northeast-1"
     vpc_name        = "terraform-vpc"
-    azs             = ["ap-northeast-3a", "ap-northeast-3b"]
+    azs             = ["ap-northeast-1a", "ap-northeast-1b"]
     public_subnets  = ["141.0.1.0/24", "141.0.2.0/24"]
     private_subnets = ["141.0.3.0/24", "141.0.4.0/24"]
     nat_gateways    = true

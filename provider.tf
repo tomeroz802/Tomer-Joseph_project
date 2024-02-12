4990fb7a87799e2c569dab536a87a98e94c3503f
tomeroz802 <95613511+tomeroz802@users.noreply.github.com>
2024-02-12 15:46:35 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,25 +1,25 @@
-provider "aws" {
-  region = var.networking.region
-}
+#provider "aws" {
+ # region = var.networking.region
+#}
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-terraform {
-  required_version = ">= 1.0.0"
+#provider "helm" {
+  #kubernetes {
+  #  config_path = "~/.kube/config"
+ # }
+#}
+#terraform {
+  #required_version = ">= 1.0.0"
 
-  required_providers {
+  #required_providers {
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.18.0"
-    }
+    #aws = {
+      #source  = "hashicorp/aws"
+     # version = ">= 4.18.0"
+    #}
 
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
-    }
-  }
-}
+    #helm = {
+    #  source  = "hashicorp/helm"
+   #   version = ">= 2.0.0"
+  #  }
+ # }
+#}
